5fe93a89acd2b5f94f304c9dfc93950902b8954e
alievaleila <meliyeva452@gmail.com>
2025-11-26 19:14:35 +0400

    initial commit

--- src/main/java/az/edu/itbrains/ecommerce/model/Category.java
+++ src/main/java/az/edu/itbrains/ecommerce/model/Category.java
@@ -10,7 +10,6 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
 
---
+++ src/main/java/az/edu/itbrains/ecommerce/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package az.edu.itbrains.ecommerce.services.impl;
+
+import az.edu.itbrains.ecommerce.services.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+}
