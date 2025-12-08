e487b28f1a3b9546dbd5cd5b70f2045f7410e1b9
alievaleila <meliyeva452@gmail.com>
2025-12-08 23:58:39 +0400

    initial commit

--- src/main/java/az/edu/itbrains/ecommerce/services/impl/ProductServiceImpl.java
+++ src/main/java/az/edu/itbrains/ecommerce/services/impl/ProductServiceImpl.java
@@ -2,13 +2,21 @@ package az.edu.itbrains.ecommerce.services.impl;
 
 import az.edu.itbrains.ecommerce.dtos.product.ProductDetailDto;
 import az.edu.itbrains.ecommerce.dtos.product.ProductHomeDto;
+import az.edu.itbrains.ecommerce.exceptions.ResourceNotFoundException;
+import az.edu.itbrains.ecommerce.model.Product;
+import az.edu.itbrains.ecommerce.repositories.ProductRepository;
 import az.edu.itbrains.ecommerce.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
     @Override
     public List<ProductHomeDto> getAllHomeProducts() {
         return List.of();
@@ -16,6 +24,10 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDetailDto getProductDetail(Long id) {
+        Product product=productRepository.findById(id).orElseThrow(()->new ResourceNotFoundException(id,"Product"));
+        if(product!=null){
+            return modelMapper.map(product,ProductDetailDto.class);
+        }
         return null;
     }
 }
