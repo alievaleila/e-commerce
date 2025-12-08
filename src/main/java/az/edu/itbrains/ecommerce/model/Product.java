0aef0210c9115a99a8cab29a07357a2a8d0463f5
alievaleila <meliyeva452@gmail.com>
2025-12-08 22:14:24 +0400

    initial commit

--- src/main/java/az/edu/itbrains/ecommerce/model/Product.java
+++ src/main/java/az/edu/itbrains/ecommerce/model/Product.java
@@ -10,12 +10,22 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(length = 1000)
     private String description;
     private double price;
     private double discount;
     private String shortDescription;
     private String barcode;
+
+    public Product(String barcode, Category category, String description, double discount,
+                   double price, String shortDescription) {
+        this.barcode = barcode;
+        this.category = category;
+        this.description = description;
+        this.discount = discount;
+        this.price = price;
+        this.shortDescription = shortDescription;
+    }
 
     @ManyToOne
     private Category category;
