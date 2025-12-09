885d37d747b92d438025df4e53a9ccadde6d4022
alievaleila <meliyeva452@gmail.com>
2025-12-09 23:30:55 +0400

    initial commit

--- src/main/java/az/edu/itbrains/ecommerce/dtos/product/ProductCreateDto.java
+++ src/main/java/az/edu/itbrains/ecommerce/dtos/product/ProductCreateDto.java
@@ -2,35 +2,27 @@ package az.edu.itbrains.ecommerce.dtos.product;
 
 import az.edu.itbrains.ecommerce.dtos.color.ColorDto;
 import az.edu.itbrains.ecommerce.dtos.photo.PhotoDto;
-import az.edu.itbrains.ecommerce.model.Category;
-import az.edu.itbrains.ecommerce.model.Color;
-import az.edu.itbrains.ecommerce.model.Photo;
 import jakarta.persistence.Column;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Size(min=5,max=50,message="Name must be at least 5 characters and at most 50 characters.")
 public class ProductCreateDto {
     private String name;
     @Column(length = 1000)
     private String description;
+    @NumberFormat
     private double price;
     private double discount;
     private String shortDescription;
     private String specification;
+    @Size(min=8,max=8)
     private String barcode;
-
-
+    @NotBlank(message = "Category can not be null")
     private Long categoryId;
 
     private List<ColorDto> colors = new ArrayList<>();
